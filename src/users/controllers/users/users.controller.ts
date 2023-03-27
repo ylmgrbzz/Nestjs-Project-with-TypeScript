6cfd5bfcfc558b4557b5553b661cbc374bf001c0
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 20:10:49 +0300

    NestJS Project

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    getUsers(@Query('sortBy') sortBy: string) {
+        console.log(sortBy);
+        return [{
+            username: 'yalım',
+            email: 'yalım@gmail.com',
+        }]
+    }
+
+    // @Get('posts')
+    // getUsersPosts() {
+    //     return [{
+    //         username: 'yalım',
+    //         email: 'yalım@gmail.com',
+    //         posts: [
+    //             {
+    //                 id: 1,
+    //                 title: 'yalım',
+    //             },
+    //             {
+    //                 id: 2,
+    //                 title: 'yalım2',
+    //             }
+    //         ]
+    //     }
+    //     ]
+    // }
+
+    // @Post('create')
+    // createUser(@Req() request: Request, @Res() response: Response) {
+    //     console.log(request.body);
+    //     response.send('User created successfully');
+    //     return {
+    //         message: 'User created successfully'
+    //     }
+    // }
+
+    @Post('create')
+    createUser(@Body() userData: CreateUserDto) {
+        console.log(userData);
+        return {
+            message: 'User created successfully'
+        }
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: string) {
+        console.log(id);
+        return { id };
+    }
+}
